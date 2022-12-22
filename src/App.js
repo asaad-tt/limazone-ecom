1572cd91fa7d6547bb82bdb699e143d3b05d925b
asaad-tt <aasadshumon86@gmail.comm>
2022-12-22 10:17:23 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
+import Home from "./Home";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route
+          path="/singleproduct/:id"
+          element={<SingleProduct></SingleProduct>}
+        ></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+        <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
